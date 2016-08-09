e45df9894bf171120d9f52dafd3f0ad0117b9761
Richard Shelton-Davies <info@allcomputersolutions.co.uk>
2016-08-09 07:36:41 +0100

    no message

---
+++ Contoso University Application/DAL/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using Contoso_University_Application;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Contoso_University_Application.DAL
+{
+    public class SchoolContext:DbContext 
+    {
+        public SchoolContext() : base("SchoolContext")
+        {
+        }
+        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
